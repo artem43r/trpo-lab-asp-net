b1adb84189599cc032e59adb1a2067690cb1a421
Artyom Zalin <z.lasekofeun@gmail.com>
2026-04-02 03:48:12 +0300

    Добавьте файлы проекта.

---
+++ laba1/Controllers/CinemaController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace laba1.Controllers
+{
+    [Route("cinema")]
+    public class CinemaController : Controller
+    {
+        // /cinema
+        [Route("")]
+        public IActionResult NowPlaying()
+        {
+            ViewBag.Title = "Сейчас в прокате";
+
+            ViewBag.Movies = new[]
+            {
+                new { Id = 1, Name = "Дюна 2" },
+                new { Id = 2, Name = "Аватар 3" },
+                new { Id = 3, Name = "Оппенгеймер" }
+            };
+
+            return View();
+        }
+
+        // /cinema/movie/1
+        [Route("movie/{movieId}")]
+        public IActionResult Movie(int movieId)
+        {
+            ViewBag.MovieId = movieId;
+            ViewBag.MovieName = $"Фильм #{movieId}";
+
+            ViewData["Rating"] = (movieId % 5) + 1;
+            ViewData["Duration"] = 120 + movieId;
+
+            return View();
+        }
+
+        // /cinema/sessions/2026-02-20
+        [Route("sessions/{date}")]
+        public IActionResult Sessions(DateTime date)
+        {
+            ViewBag.Date = date.ToShortDateString();
+
+            ViewBag.Sessions = new[]
+            {
+                "12:00",
+                "15:30",
+                "18:00",
+                "21:00"
+            };
+
+            return View();
+        }
+
+        // Кастомный маршрут
+        [Route("today")]
+        public IActionResult Today()
+        {
+            string today = DateTime.Today.ToString("yyyy-MM-dd");
+            return Redirect($"/cinema/sessions/{today}");
+        }
+
+    }
+}
---
+++ laba1/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using laba1.Models; //Чтобы видел Product
+
+namespace laba1.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+        IEnumerable<Product> GetByCategory(string category);
+        IEnumerable<Product> GetInStock();
+    }
+}
